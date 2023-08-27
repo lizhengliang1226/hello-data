d590b9fd56ea8cae316fcc50576e31e190531450
lizhengliang1226 <1731415266@qq.com>
2023-08-27 17:40:00 +0800

    mod

--- src/main/java/com/lzl/datagenerator/config/Configuration.java
+++ src/main/java/com/lzl/datagenerator/config/Configuration.java
@@ -1,13 +1,16 @@
 package com.lzl.datagenerator.config;
 
+import cn.hutool.db.DbUtil;
 import cn.hutool.db.meta.JdbcType;
 import cn.hutool.setting.yaml.YamlUtil;
 import lombok.Data;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -34,17 +37,59 @@ public class Configuration {
             synchronized (DataConfigBean.class) {
                 // 第二次检查，保证只有一个实例被创建
                 if (CONFIGURATION == null) {
-                    CONFIGURATION=new Configuration();
+                    CONFIGURATION = new Configuration();
                     CONFIGURATION = YamlUtil.loadByPath("classpath:/config/generate.yml", Configuration.class);
-                    CONFIGURATION.getDataConfigList().parallelStream().filter(
-                                         config -> "ALL".equals(CONFIGURATION.getGenerate()) || CONFIGURATION.getGenerate().contains(config.getDataSourceId()))
+                    CONFIGURATION.getDataConfigList()
+                                 .parallelStream()
+                                 .filter(config -> "ALL".equals(CONFIGURATION.getGenerate()) || CONFIGURATION.getGenerate()
+                                                                                                             .contains(config.getDataSourceId()))
                                  .forEach(DataConfigBean::init);
                     CONFIGURATION.initJdbcTypeDefaultValue();
+                    CONFIGURATION.writeConfigToDb();
                 }
             }
         }
         return CONFIGURATION;
     }
+
+    private void writeConfigToDb() {
+        dataConfigList.stream().forEach(dc -> {
+            Map<String, String> colDefaultValue = dc.getColDefaultValue();
+            dc.getColumnConfigMap().forEach((k, v) -> {
+//                ColumnConfig columnConfig = new ColumnConfig();
+//                columnConfig.setDatasourceId(v.getDataSourceId());
+//                columnConfig.setColumnName(v.getColName());
+//                columnConfig.setDefaultVal(colDefaultValue.getOrDefault(k, " "));
+//                columnConfig.setStrategyName(v.getStrategy());
+//                Number baseValue = v.getBaseValue();
+//                if (baseValue == null) {
+//                    columnConfig.setBaseValue(0L);
+//                } else {
+//                    columnConfig.setBaseValue(baseValue.longValue());
+//                }
+//                columnConfig.setPrefix(v.getPrefix());
+//                columnConfig.setSuffix(v.getSuffix());
+//                columnConfig.setStep(v.getStep());
+//                columnConfig.setQuerySql(v.getQuerySql());
+//                columnConfig.setQueryCol(v.getQueryCol());
+//                columnConfig.setRandomEle(v.getRandomEle().stream().map(o->String.valueOf(o)).collect(Collectors.joining(",")));
+//                columnConfig.setDictColName(v.getDictColName());
+//                columnConfig.setFixedValue(Objects.toString(v.getFixedValue(),""));
+                String a="insert into column_config (DATASOURCE_ID, COLUMN_NAME, STRATEGY_NAME)\n" + "values ('%s','%s','%s')";
+                try {
+                    DbUtil.use(DbUtil.getDs("mysql"))
+                          .execute(String.format(a,v.getDataSourceId(),v.getColName(),v.getStrategy()));
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        });
+    }
+
+    public String getValue(Object o, Object def) {
+        return String.valueOf(Optional.ofNullable(o).orElse(def));
+    }
+
     private void initJdbcTypeDefaultValue() {
         typeDefaultValMap = jdbcTypeDefaultValue.entrySet().parallelStream().collect(Collectors.toMap(e -> JdbcType.valueOf(e.getKey()), e -> {
             if (placholderMap.containsKey(e.getValue())) {
---
+++ src/main/java/entity/Strategy.java
@@ -0,0 +1,15 @@
+package entity;
+
+import lombok.Data;
+
+/**
+ * @author LZL
+ * @version 1.0
+ * @since 2023/8/27
+ */
+@Data
+public class Strategy {
+    private String strategyId;
+    private String strategyCode;
+    private String strategyName;
+}
